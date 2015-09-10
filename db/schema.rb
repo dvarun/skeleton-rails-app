1832b59ddfa44e2b1fa3dc5c2c40b1e42983e31d
Varun Dubey <dvarun@hotmail.com>
2015-09-11 02:08:35 +0700

    bug fixes and new models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150827190815) do
+ActiveRecord::Schema.define(version: 20150908142254) do
+
+  create_table "permissions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-skeleton-rails-app
+## Skeleton Rails Application
+
+This application is bundled with devise and cancan gem, to get started with creating application which requires user authentication and authorization.
+
+more details coming soon.
