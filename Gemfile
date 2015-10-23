9f506c93484473a367ce96427b7a287b2b292923
Varun Dubey <dvarun@hotmail.com>
2015-10-23 21:59:55 +0700

    installed cancan with bug workaround added

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 #devise
 gem 'devise'
+#cancan
+gem "cancan"
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+ # Prevent CSRF attacks by raising an exception.
+ # For APIs, you may want to use :null_session instead.
+ protect_from_forgery with: :exception
+
+ rescue_from CanCan::AccessDenied do |exception|
+  redirect_to root_url, alert: exception.message
+ end
+
+ #### must add if using cancan otherwise no data will get inserted/updated
+ before_filter do
+  resource = controller_name.singularize.to_sym
+  method = "#{resource}_params"
+  params[resource] &&= send(method) if respond_to?(method, true)
+ end
+ #### end of cancan workaround
+
 end
